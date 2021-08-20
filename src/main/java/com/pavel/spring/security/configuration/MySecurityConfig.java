19467ed422e56821a659b2d860bceed01c7540d9
pavel <natitshow@gmail.com>
2021-08-20 08:16:18 +0300

    добавил простую авторизацию и аутентификацию

--- src/main/java/com/pavel/spring/security/configuration/MySecurityConfig.java
+++ src/main/java/com/pavel/spring/security/configuration/MySecurityConfig.java
@@ -2,16 +2,18 @@ package com.pavel.spring.security.configuration;
 
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
 
 @EnableWebSecurity
 public class MySecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        User.UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
         auth.inMemoryAuthentication()
                 .withUser(userBuilder.username("pasha")
                         .password("pasha")
@@ -24,6 +26,14 @@ public class MySecurityConfig extends WebSecurityConfigurerAdapter {
                         .username("ivan")
                         .password("ivan")
                         .roles("MANAGER, HR"));
-        super.configure(auth);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/")
+                .hasAnyRole("EMPLOYEE","HR", "MANAGER")
+                .antMatchers("/hr_info").hasAnyRole("HR")
+                .antMatchers("/manager_info").hasAnyRole("MANGER")
+                .and().formLogin().permitAll();
     }
 }
---
+++ src/main/webapp/WEB-INF/view/view_for_hr.jsp
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h3>Here you can see salary </h3>
+
+
+
+
+</body>
+</html>
